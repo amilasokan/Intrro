0462c88ef74ba5131a774ac6d46bd7804de3e74b
Amil Asokan <amilasokan@gmail.com>
2019-09-07 00:12:42 +0400

    Final Commit

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@ let vm = new Vue({
     backIcon: function() {
       return (this.orientation === 'row' ? 'fa-arrow-left' : 'fa-arrow-up')
     },
-    
+
   },
   watch: {
     orientation: 'setCssVars',
@@ -124,5 +124,39 @@ let vm = new Vue({
     finish: function(){
       this.toggleModal()
     }
-  } 
+  }
 })
+
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+		$('.image-upload-wrap').addClass('image-dropping');
+	});
+	$('.image-upload-wrap').bind('dragleave', function () {
+		$('.image-upload-wrap').removeClass('image-dropping');
+});
